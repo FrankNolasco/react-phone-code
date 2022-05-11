71608da71ae5e73fb3adc51f4d0e89cc2f36e10e
FranksitoKawai <fnv1298@gmail.com>
2022-05-11 11:44:01 -0500

    last version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,12 +3,11 @@ import { Country } from './data/Paises';
 import { ThemeProps } from './data/Theme';
 export declare type TypeValueExpected = Country | undefined;
 interface Props {
-    value: TypeValueExpected;
+    value?: TypeValueExpected;
     onChange: (value: TypeValueExpected) => void;
     disableModal?: boolean;
     size?: SizeType;
     theme?: ThemeProps;
-    darkMode?: boolean;
 }
 export declare const PhoneCode: ({ value, onChange, disableModal, size, theme, }: Props) => JSX.Element;
 export {};
